b2e9fefef326aea87434aebccdcfb9baf495c307
André Fernandes dos Santos <andrefs@cpan.org>
2012-03-24 19:11:27 +0000

    Import of ANDREFS/Task-BeLike-Andrefs-0.05_12 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-Andrefs
    gitpan-cpan-version:      0.05_12
    gitpan-cpan-path:         ANDREFS/Task-BeLike-Andrefs-0.05_12.tar.gz
    gitpan-cpan-author:       ANDREFS
    gitpan-cpan-maturity:     developer

--- lib/Task/BeLike/Andrefs.pm
+++ lib/Task/BeLike/Andrefs.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package Task::BeLike::Andrefs;
 {
-  $Task::BeLike::Andrefs::VERSION = '0.05_03';
+  $Task::BeLike::Andrefs::VERSION = '0.05_12';
 }
 
 # ABSTRACT: For all the AndreFS wanna-bes
@@ -19,7 +19,7 @@ Task::BeLike::Andrefs - For all the AndreFS wanna-bes
 
 =head1 VERSION
 
-version 0.05_03
+version 0.05_12
 
 =head1 AUTHOR
 
